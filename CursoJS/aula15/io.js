86032fe49cdf55ca73b914bd1f5b76b6c24c4382
BRUNO HENRIQUE DE LIMA JUSTINIANO <121064827+BrunoJustiniano@users.noreply.github.com>
2022-12-25 20:03:15 -0300

    Add files via upload
    curso Java Script / curso em video

---
+++ CursoJS/aula15/io.js
@@ -0,0 +1,8 @@
+var ini = 5;
+var fim = 12;
+var inc = 2;
+
+while (ini <= fim) {
+  console.log(ini);
+  ini = ini + inc;
+}
---
+++ CursoJS/aula16/funcao02.js
@@ -0,0 +1,12 @@
+/*function soma(n1, n2) {
+  return n1 + n2;
+}
+
+console.log(soma(2, 5));*/
+
+function soma(n1 = 0, n2 = 0) {
+  // definier parametro antes de receber a chamada.
+  return n1 + n2;
+}
+
+console.log(soma(7));
